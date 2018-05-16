f8797e6236b33a6bddb42f6daf945478fd82de45
Benjamin Kisliuk <benjamin.kisliuk@dfki.de>
2018-05-16 14:31:41 +0200

    initial commit

---
+++ src/transformer_py.cc
@@ -0,0 +1,17 @@
+#include <Transformer.h>
+#include <boost/python.hpp>
+
+typedef esrocos::transformer::AcyclicTransformer<20,20> atf;
+
+BOOST_PYTHON_MODULE(transformer_py)
+{
+  using namespace boost::python;
+
+  const char * (atf::Frame::*id_get)() const = &atf::Frame::id;
+  bool (atf::Frame::*id_set)(const char *) = &atf::Frame::id;
+
+  class_<atf::Frame>("Frame", init<const char *>())
+    .def("id",id_get)
+    .def("id",id_set)
+    ;
+}
